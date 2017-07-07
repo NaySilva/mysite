f268fa9e904522c20ec05fe16460cf6513294a54
Nayara Silva <nayara.silva.f@hotmail.com>
2017-07-07 18:01:27 -0300

    parte do slide django_04 e 05

--- polls/urls.py
+++ polls/urls.py
@@ -6,4 +6,10 @@ from polls import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^question/(?P<question_id>\d+)$', views.exibir, name='exibir'),
+    url(r'^question/(?P<question_id>\d+)/result$', views.result, name='result'),
+    url(r'^question/(?P<question_id>\d+)/vote$', views.vote, name='vote'),
+    url(r'^choice/(?P<choice_id>\d+)/votar$', views.votar, name='votar'),
+    url(r'^question/(?P<question_id>\d+)/manage$', views.manage, name='manage'),
+    url(r'^question/(?P<question_id>\d+)/mudar_status$', views.mudar_status, name='mudar_status'),
+    url(r'^choice/(?P<choice_id>\d+)/remove$', views.remove, name='remove'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,47 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from polls.models import Question, Choice
 
 
 def index(request):
-    questions = Question.objects.all()
+    questions = Question.objects.order_by('-pub_date')
     return render(request, 'index.html', {'questions':questions})
 
 def exibir(request, question_id):
     question = Question.objects.get(id=question_id)
     choices = Choice.objects.filter(question=question)
     return render(request, 'question.html', {'question': question, 'choices':choices})
+
+def result(request, question_id):
+    question = Question.objects.get(id=question_id)
+    choices = Choice.objects.filter(question=question)
+    return render(request, 'result.html', {'question': question, 'choices':choices})
+
+def vote(request, question_id):
+    question = Question.objects.get(id=question_id)
+    choices = Choice.objects.filter(question=question)
+    return render(request, 'votar.html', {'question': question, 'choices':choices})
+
+def votar(request, choice_id):
+    choice = Choice.objects.get(id=choice_id)
+    choice.votes += 1
+    choice.save(force_update=True)
+    return redirect('index')
+
+def remove(request, choice_id):
+    choice = Choice.objects.get(id=choice_id)
+    choice.delete()
+    return redirect('index')
+
+def mudar_status(request, question_id):
+    question = Question.objects.get(id=question_id)
+    question.closed = not question.closed
+    question.save(force_update=True)
+    return redirect('index')
+
+def manage(request, question_id):
+    question = Question.objects.get(id=question_id)
+    choices = Choice.objects.filter(question=question)
+    return render(request, 'manage.html', {'question': question, 'choices': choices})
